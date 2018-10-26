fb0c352c7a2a25698f91dd078a85ede175791472
Eduardo <eduardoheyder@gmail.com>
2018-10-26 10:32:27 -0300

    adicionadas instruções de comandos

--- index.php
+++ index.php
@@ -8,6 +8,19 @@
 
 	echo "nova linha de código";
 	
+	//conectar ao xampp primeiro de tudo!
+
+	// esquema para mandar e receber linhas de código pro sistema:
+
+	//alterar/adicionar na linha de código PHP no sublime e salvar.
+
+	//no cmd do windows fazer o comando add --all
+
+	//depois o comando commit -m para adicionar a mensagem de alteração
+
+	//enviar o código para o github com o push origin
+
+	//receber o código atualizado com pull origin
 
 
 ?>
